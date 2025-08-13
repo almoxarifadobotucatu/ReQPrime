f16b43c5104444b8251d5614274c656a0518b62b
almoxarifadobotucatu <pedidoextrapirangi@gmail.com>
2025-08-13 20:20:14 -0300

    Update protege-pagina.js

--- protege-pagina.js
+++ protege-pagina.js
@@ -1,17 +1,21 @@
-// protege-pagina.js
 (function() {
   const paginasProtegidas = [
-    "/central-de-pedidos/",
-    "/pedidoextra/",
-    "/pedidomensal/"
+    "central-de-pedidos",
+    "pedidoextra",
+    "pedidomensal"
   ];
 
-  const caminhoAtual = window.location.pathname;
+  const caminhoAtual = window.location.pathname.toLowerCase();
 
-  // Se a URL atual for de uma página protegida
-  if (paginasProtegidas.some(p => caminhoAtual.startsWith(p))) {
+  // Detecta se está em alguma das páginas protegidas
+  if (paginasProtegidas.some(p => caminhoAtual.includes("/" + p + "/"))) {
     if (!sessionStorage.getItem("logado")) {
-      window.location.href = "/login/index.html";
+      // Volta para a pasta /login relativa ao nível atual
+      if (caminhoAtual.endsWith("/")) {
+        window.location.href = "../login/index.html";
+      } else {
+        window.location.href = "../../login/index.html";
+      }
     }
   }
 })();
